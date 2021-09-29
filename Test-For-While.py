0d35c964521e00e2cb6eee2d40272428cf1418ce
Fusilladin <91526142+Fusilladin@users.noreply.github.com>
2021-09-29 18:35:32 -0500

    Add files via upload

---
+++ Test-For-While.py
@@ -0,0 +1,26 @@
+# nums = [1, 2, 3, 4, 5]
+
+# for num in nums:
+#     if num == 3:
+#         print('Found {}!'.format(num))
+#         continue #break
+#     print(num)
+
+# for num in nums:
+#     for letter in 'abc':
+#         print(num, letter)
+
+##################
+
+# for i in range(1, 11):
+#     print(i)
+
+#####################
+
+x = 0
+
+while True:
+    print(x)
+    if x == 5:
+        break
+    x += 1
